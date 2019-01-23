982da727d0db126fad51c34accdeeb68bffa0889
pokutuna <popopopopokutuna@gmail.com>
2019-01-23 11:36:51 +0900

    setup

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+HERE=$(cd $(dirname "$0") && pwd)
+TARGET=${TARGET:-~/Library/Application Support/Code/User}
+FILES=$(ls "${HERE}" | grep -v $(basename "$0"))
+for file in $FILES; do
+    ln -s "${HERE}/${file}" "${TARGET}/${file}"
+done
