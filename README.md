626e45b1982ca2b39ba8975dffaaac39d077a3ec
pokutuna <popopopopokutuna@gmail.com>
2019-01-23 11:43:12 +0900

    doc

---
+++ README.md
@@ -0,0 +1,8 @@
+vscode-settings
+===
+
+`$ ./setup.sh` makes symlink to `~/Library/Application Support/Code/User`
+
+or
+
+`$ TARGET=/path/to/setting/directory ./setup.sh`
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 HERE=$(cd $(dirname "$0") && pwd)
 TARGET=${TARGET:-~/Library/Application Support/Code/User}
-FILES=$(ls "${HERE}" | grep -v $(basename "$0"))
+FILES=$(ls "${HERE}" | grep -v $(basename "$0") | grep -v README.md)
 for file in $FILES; do
-    ln -s "${HERE}/${file}" "${TARGET}/${file}"
+    ln -s "${HERE}/${file}" "${TARGET}/"
 done
