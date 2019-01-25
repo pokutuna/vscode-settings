abb1d8cd50799282f3295f4ea3325fe00c852736
pokutuna <popopopopokutuna@gmail.com>
2019-01-25 12:12:09 +0900

    output

--- list-extensions.sh
+++ list-extensions.sh
@@ -3,4 +3,5 @@
 HERE=$(cd $(dirname "$0") && pwd)
 # from https://stackoverflow.com/questions/35773299/how-can-you-export-vs-code-extension-list
 code --list-extensions | xargs -L 1 echo code --install-extension > ${HERE}/install-extensions.sh
+cat ${HERE}/install-extensions.sh
 chmod +x ${HERE}/install-extensions.sh
