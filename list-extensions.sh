2b0afd6b7207d01f9e33ce29cd93ba5a07a0fda5
pokutuna <popopopopokutuna@gmail.com>
2019-01-25 12:02:54 +0900

    add link

--- list-extensions.sh
+++ list-extensions.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 HERE=$(cd $(dirname "$0") && pwd)
+# from https://stackoverflow.com/questions/35773299/how-can-you-export-vs-code-extension-list
 code --list-extensions | xargs -L 1 echo code --install-extension > ${HERE}/install-extensions.sh
 chmod +x ${HERE}/install-extensions.sh
